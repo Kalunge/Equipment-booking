90833e9c29902070f0e1905b7862fb4de3fed6be
kalunge <muthomititus+github@gmail.com>
2021-07-30 13:00:07 +0100

    Add logic to fetch products from the backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotEnv from 'dotenv';
+import colors from 'colors'
+import products from './data/products.js';
+
+const app = express();
+
+dotEnv.config();
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} Mode on port ${PORT}`.cyan.bold)
+);
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import {
   Col,
@@ -10,11 +11,18 @@ import {
   Button,
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
-  console.log(product.name);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  });
+
   return (
     <>
       <Link className='btn btn-light my-1 ' to='/'>
